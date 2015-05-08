fe851b65e16d5a828fe99f23ed52b803c0f8ab40
maplen76 <jingwang512@gmail.com>
2015-05-08 22:14:52 +0800

    Revert "complete 80% coding job"
    This reverts commit e6f27f554acf858b1d3bc90753537c1c0dc11802.

--- Exploratory Data Analysis Course Project 1.R
+++ Exploratory Data Analysis Course Project 1.R
@@ -10,44 +10,4 @@ unzip(zipfile = zip_Path, exdir = getwd())
 epc_path <- paste(getwd(),"/", "household_power_consumption.txt", sep = "")
 
 # read the data into R
-epc <- read.table(file = epc_path, header = T, sep = ";", stringsAsFactors = F)
-epc$dt <- as.Date(epc$Date, format = "%d/%m/%Y")
-head(epc)
-
-# subset the data between 2007-02-01 and 2007-02-02
-epc_plot <- subset(epc, dt >= '2007-02-01' & dt <= '2007-02-02')
-epc_plot$date_time <- strptime(paste(epc_plot$Date, epc_plot$Time), "%d/%m/%Y %H:%M:%S")
-
-epc_plot$Global_active_power <- as.numeric(epc_plot$Global_active_power)
-
-# Plot1
-png(file = "Plot1.png", width = 480, height = 480, units = "px")
-hist(as.numeric(epc_plot$Global_active_power), 
-     col = "red"
-     ,main = "Global Active Power"
-     ,xlab = "Global Active Power (kilowatts)")
-dev.off()
-
-
-# Plot2
-with(epc_plot, plot(date_time, Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)"))
-
-# Plot3
-with(epc_plot, plot(date_time, Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = ""))
-with(epc_plot, lines(date_time, Sub_metering_2, type = "l", col = "red"))  
-with(epc_plot, lines(date_time, Sub_metering_3, type = "l", col = "blue"))
-legend("topright", lty=c(1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-
-#Plot4
-par(mfrow = c(2, 2))
-with(epc_plot,{ 
-    plot(date_time, Global_active_power, type = "l",  ylab = "Global Active Power", xlab = "")
-    plot(date_time, as.numeric(Voltage), type = "l",  ylab = "Voltage", xlab = "datetime")
-    plot(date_time, Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab = "")
-    lines(date_time, Sub_metering_2, type = "l", col = "red")
-    lines(date_time, Sub_metering_3, type = "l", col = "blue")
-    legend("topright", bty = 'n', lty=c(1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-    plot(date_time, as.numeric(Global_reactive_power), type = "l", ylab = "Global_reactive_power" )
-                })
-
+epc <- read.table(file = epc_path, sep = ";", stringsAsFactors = F)
